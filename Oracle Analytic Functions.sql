7e941cf3724b84985376cb3032341c4186cda61c
Monir <monir@therapservices.net>
2018-04-16 15:43:56 +0600

    Intro to window functions

--- Oracle Analytic Functions.sql
+++ Oracle Analytic Functions.sql
@@ -80,3 +80,33 @@
       s.time_id  = t.time_id AND calendar_year = 1998
     GROUP BY
       t.calendar_month_number;
+  
+--                                               ########       Window Functions   ########
+
+/*
+ Analytic functions compute an aggregate value based on a group of rows. They differ from aggregate functions in that they return multiple rows for each group.
+ Analytic functions also operate on subsets of rows, similar to aggregate functions in GROUP BY queries, but they do not reduce the number of rows returned by the query.
+ 
+ The group of rows is called a WINDOW and is defined by the analytic clause. For each row, a "sliding" window of rows is defined. 
+ The window determines the range of rows used to perform the calculations for the "current row". 
+ Window sizes can be based on either a physical number of rows or a logical interval such as time.
+
+ Select MAX() OVER ()
+ 
+   Here, The OVER() statement signals a start of an Analytic function. That is what differentiates an Analytical Function from a regular Oracle SQL function.
+
+ Select MAX() OVER(partition by field1)
+ 
+   The portioning clause is used to setup the group of data that the Analytic function would be applied to.
+
+ Select MAX() OVER(Partition by field order by)
+   
+   Order by specify the order of the window in the group by statement. The Order by clause is a keyword in the Oracle Analytic syntax that is requirement for using some Analytic functions
+
+ Analytic functions are the last set of operations performed in a query except for the final ORDER BY clause.
+
+ All joins and all WHERE, GROUP BY, and HAVING clauses are completed before the analytic functions are processed. 
+ Therefore, analytic functions can appear only in the select list or ORDER BY clause.
+
+*/
+
