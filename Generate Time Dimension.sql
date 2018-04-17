499d7ab61abd458ca511404e45baa7d9c867802f
Md Moniruzzaman <38385928+Escapist-007@users.noreply.github.com>
2018-04-17 13:22:11 +0600

    Update Generate Time Dimension.sql

--- Generate Time Dimension.sql
+++ Generate Time Dimension.sql
@@ -1,7 +1,10 @@
-  /*    TIME DIMENSION CREATION */
+/*
+   Author  : Md Moniruzzaman Monir
+*/
 
- CREATE
-  TABLE DIM_TIME
+/*    TIME DIMENSION CREATION */
+
+ CREATE TABLE DIM_TIME
   (
     ID         NUMBER ( 10 ) PRIMARY KEY,
     BEGIN_DATE DATE,
@@ -11,39 +14,38 @@
     QUARTER    NUMBER ( 1 ),
     YEAR       VARCHAR2 ( 4 ),
     YEAR_INT   NUMBER ( 4 )
-  )
+  );
 
-  
   
     /*       INSERT MONTHLY (MONTH - GRANULARITY)    */
 
- INSERT INTO 
-       DIM_TIME (ID, Begin_Date, End_Date, Month, Month_Int, Quarter, Year, Year_Int) 
 
- WITH TIME_RANGE AS 
- (
-  SELECT
-     TO_DATE ( '01-Jan-2016', 'DD-MON-YYYY' ) Start_Date, -- 01 JANUARY, 2016
-     SYSDATE End_Date
-  FROM
-     DUAL
- )
- SELECT
-       TO_NUMBER (TO_CHAR(ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1)),'YYYYQMM')) ID,
+   INSERT INTO 
+         DIM_TIME (ID, Begin_Date, End_Date, Month, Month_Int, Quarter, Year, Year_Int) 
 
-       ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1))             Month_Begin_Date,
-       LAST_DAY( ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1)) ) Month_End_Date,
+   WITH TIME_RANGE AS 
+   (
+    SELECT
+       TO_DATE ( '01-Jan-2016', 'DD-MON-YYYY' ) Start_Date, -- 01 JANUARY, 2016
+       SYSDATE End_Date
+    FROM
+       DUAL
+   )
+   SELECT
+         TO_NUMBER (TO_CHAR(ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1)),'YYYYQMM')) ID,
 
-       to_char( ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1)),'MON' )           Month , 
-       to_number( to_char(ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1)),'MM'))  Month_Int,
+         ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1))             Month_Begin_Date,
+         LAST_DAY( ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1)) ) Month_End_Date,
 
-       to_char(ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1)),'q')               Quarter,   
-       to_char(ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1)),'YYYY')            Year,
-       to_number(to_char(ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1)),'YYYY')) Year_Int
+         to_char( ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1)),'MON' )           Month , 
+         to_number( to_char(ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1)),'MM'))  Month_Int,
 
- FROM TIME_RANGE
- CONNECT BY ( LEVEL - 1 ) <= MONTHS_BETWEEN ( SYSDATE, TIME_RANGE.Start_Date);
-  
+         to_char(ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1)),'q')               Quarter,   
+         to_char(ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1)),'YYYY')            Year,
+         to_number(to_char(ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1)),'YYYY')) Year_Int
+
+   FROM TIME_RANGE
+   CONNECT BY ( LEVEL - 1 ) <= MONTHS_BETWEEN ( SYSDATE, TIME_RANGE.Start_Date);
   
   
   
@@ -67,8 +69,8 @@
         ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1)*3)   Quarter_Begin_Date,
 
         CASE 
-          WHEN TO_CHAR (SYSDATE, 'YYYYQ') = TO_CHAR ( LAST_DAY ( ADD_MONTHS ( TIME_RANGE.Start_Date,(LEVEL-1)*3 + 2) ), 'YYYYQ') 
-            THEN LAST_DAY (SYSDATE)
+          WHEN TO_CHAR (SYSDATE, 'YYYYQ') = TO_CHAR (LAST_DAY(ADD_MONTHS(TIME_RANGE.Start_Date,(LEVEL-1)*3 + 2) ), 'YYYYQ') 
+              THEN LAST_DAY (SYSDATE)
           ELSE 
              LAST_DAY( ADD_MONTHS ( TIME_RANGE.Start_Date,(LEVEL-1)*3 + 2) )
         END Quarter_END_DATE,
@@ -82,9 +84,6 @@
 
   FROM TIME_RANGE
   CONNECT BY ( LEVEL - 1 ) <= MONTHS_BETWEEN ( SYSDATE, TIME_RANGE.Start_Date)/3;
- 
- 
- 
  
  
     
