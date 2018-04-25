a242536f6b49be41b009b5b57648d674529dd71d
Monir <monir@therapservices.net>
2018-04-25 11:07:32 +0600

    Update General Functions for NULL value.sql, NVL2 & NULLIF is added

--- General Functions for NULL value.sql
+++ General Functions for NULL value.sql
@@ -32,3 +32,60 @@ SELECT
   ( SALARY * 12 ) + ( SALARY * 12 * NVL ( COMMISSION_PCT, 0 ) ) ANN_SAL
 FROM
   HR.EMPLOYEES;
+  
+  
+/*
+  The NVL2 function examines the first expression. If the first expression is not null, the NVL2 function returns the second expression. 
+  If the first expression is null, the third expression is returned. 
+
+  Syntax: NVL2 (expr1, expr2, expr3)
+
+  In the syntax:
+    • expr1 is the source value or expression that may contain a null
+    • expr2 is the value that is returned if expr1 is not null
+    • expr3 is the value that is returned if expr1 is null
+  
+  The argument expr1 can have any data type. The arguments expr2 and expr3 can have any data types except LONG. 
+*/
+
+SELECT
+  DEPARTMENT_ID,
+  LAST_NAME,
+  SALARY,
+  COMMISSION_PCT,
+  NVL2 ( COMMISSION_PCT, 'SAL+COMM', 'SAL' ) INCOME
+FROM
+  HR.EMPLOYEES
+WHERE
+  DEPARTMENT_ID IN ( 50, 80 ) ;
+  
+/*
+  Syntax: NULLIF (expr1, expr2)
+
+  The NULLIF ( ) function is logically equivalent to the following CASE expression. 
+        
+        CASE 
+           WHEN expr1 = expr2 THEN NULL 
+           ELSE expr1 
+        END
+  
+   Here, If expr1 is NULL then the function will return NULL
+*/
+
+SELECT
+  FIRST_NAME,
+  LENGTH ( FIRST_NAME ) "expr1",
+  LAST_NAME,
+  LENGTH ( LAST_NAME ) "expr2",
+  NULLIF ( LENGTH ( FIRST_NAME ), LENGTH ( LAST_NAME ) ) RESULT
+FROM
+  HR.EMPLOYEES;
+
+SELECT
+  FIRST_NAME
+  ||' '
+  || LAST_NAME ENAME,
+  COMMISSION_PCT,
+  NULLIF ( COMMISSION_PCT, 0 )
+FROM
+  HR.EMPLOYEES;
